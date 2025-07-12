22abb8a71a96ebf1e9de2b5f71364557aad35181
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-07-12 21:06:50 +0330

    test location in production

--- src/components/LocationPosition.tsx
+++ src/components/LocationPosition.tsx
@@ -7,7 +7,6 @@ import { useMap } from 'react-leaflet';
 
 export default function LocationPosition() {
   const setCurrentPosition = useMapStore((state) => state.setCurrentPosition);
-  const setAccuracy = useMapStore((state) => state.setAccuracy);
   const currentPosition = useMapStore((state) => state.currentPosition);
   const setIsLoading = useGlobalStore((state) => state.setIsLoading);
   const map = useMap();
@@ -21,32 +20,21 @@ export default function LocationPosition() {
     setIsLoading(true);
     setCurrentPosition(null);
 
-    map.locate({
-      watch: true,
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 0,
-    });
-
-    map.on('locationfound', (e) => {
-      const { latlng, accuracy } = e;
-      setAccuracy(accuracy);
-
-      console.log(
-        `Latitude: ${latlng.lat}, Longitude: ${latlng.lng}, Accuracy: ${accuracy}m`,
-      );
-
-      setCurrentPosition([latlng.lat, latlng.lng]);
-      map.stopLocate();
-      setIsLoading(false);
-    });
-
-    map.on('locationerror', (e) => {
-      console.error('Error getting location:', e.message);
-      alert('Unable to retrieve your location. Please allow location access.');
-      map.stopLocate();
-      setIsLoading(false);
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        setCurrentPosition([latitude, longitude]);
+        setIsLoading(false);
+      },
+      (error) => {
+        console.error('Error getting location:', error);
+        alert(
+          'Unable to retrieve your location. Please allow location access in your browser settings.',
+        );
+        setIsLoading(false);
+      },
+    );
   };
 
   useEffect(() => {
@@ -57,18 +45,12 @@ export default function LocationPosition() {
     });
   }, [currentPosition, map]);
 
-  useEffect(() => {
-    return () => {
-      map.stopLocate();
-    };
-  }, [map]);
-
   return (
     <button
       onClick={findLocationHandler}
       className='absolute end-4 top-4 z-[999] flex size-12 cursor-pointer items-center justify-center rounded-xl bg-white shadow'
     >
-      <LocateFixed className='size-6 animate-pulse text-blue-500' />
+      <LocateFixed className='size-6 text-blue-500' />
     </button>
   );
 }
